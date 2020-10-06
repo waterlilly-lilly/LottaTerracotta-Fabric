a7994d5d6e4dbf476278b2a860eebb2073656fc2
Angela <angela@mewshroom.com>
2020-10-06 14:04:54 -0700

    Update for 1.16.3

--- src/main/java/org/villainy/lottaterracotta/tileEntities/renderers/TerracottaSignTileEntityRenderer.java
+++ src/main/java/org/villainy/lottaterracotta/tileEntities/renderers/TerracottaSignTileEntityRenderer.java
@@ -12,11 +12,10 @@ import net.minecraft.client.renderer.tileentity.SignTileEntityRenderer;
 import net.minecraft.client.renderer.tileentity.TileEntityRenderer;
 import net.minecraft.client.renderer.tileentity.SignTileEntityRenderer.SignModel;
 import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
+import net.minecraft.util.IReorderingProcessor;
 import net.minecraft.util.math.vector.Vector3f;
 import net.minecraft.state.properties.AttachFace;
 import net.minecraft.util.Direction;
-import net.minecraft.util.text.ITextProperties;
-import net.minecraft.util.text.Style;
 import org.villainy.lottaterracotta.blocks.TerracottaSignBlock;
 import org.villainy.lottaterracotta.tileEntities.TerracottaSignTileEntity;
 import org.villainy.lottaterracotta.client.Atlases;
@@ -104,15 +103,15 @@ public class TerracottaSignTileEntityRenderer extends TileEntityRenderer<Terraco
         int blue = (int) ((double) NativeImage.getBlue(color) * 0.4D);
         int adjustedColor = NativeImage.getCombined(0, blue, green, red);
 
-        for (int j1 = 0; j1 < 4; ++j1)
+        for (int line = 0; line < 4; ++line)
         {
-            ITextProperties itextproperties = tileEntity.func_235677_a_(j1, (p_212491_1_) -> {
-                List<ITextProperties> list = fontrenderer.func_238420_b_().func_238362_b_(p_212491_1_, 90, Style.EMPTY);
-                return list.isEmpty() ? ITextProperties.field_240651_c_ : list.get(0);
+            IReorderingProcessor ireorderingprocessor = tileEntity.func_242686_a(line, (text) -> {
+                List<IReorderingProcessor> list = fontrenderer.func_238425_b_(text, 90);
+                return list.isEmpty() ? IReorderingProcessor.field_242232_a : list.get(0);
             });
-            if (itextproperties != null) {
-                float f3 = (float)(-fontrenderer.func_238414_a_(itextproperties) / 2);
-                fontrenderer.func_238416_a_(itextproperties, f3, (float)(j1 * 10 - 20), adjustedColor, false, matrixStackIn.getLast().getMatrix(), bufferIn, false, 0, combinedLightIn);
+            if (ireorderingprocessor != null) {
+                float f3 = (float)(-fontrenderer.func_243245_a(ireorderingprocessor) / 2);
+                fontrenderer.func_238416_a_(ireorderingprocessor, f3, (float)(line * 10 - 20), adjustedColor, false, matrixStackIn.getLast().getMatrix(), bufferIn, false, 0, combinedLightIn);
             }
         }
 
